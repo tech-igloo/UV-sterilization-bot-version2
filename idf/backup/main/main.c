ee282ea065ce166be684c7e5387f53e6c126ce82
naruarjun <naruarjun@gmail.com>
2020-09-20 22:20:46 +0530

    Adding basic functions for obstacle avoidance

--- idf/backup/main/main.c
+++ idf/backup/main/main.c
@@ -69,6 +69,239 @@ static int auto_flag = 0;                       //Denote whether auto mode is on
 static ledc_channel_config_t led_channel;       //Data Structure having various fields specifying the PWM details for a single channel. A single channel for a single PWM output. Will have to create another for controlling 2 motors
 static TaskHandle_t Task1;                      //Task handle to keep track of created task running on Core 1
 
+/*Variables for obstacle avoidance*/
+static double* points = NULL;                   //stores the points in the path
+static int points_len = 0;                      //length of the path
+static double prev_time = 0;                    //stores the previous time step, gets updated to current time in sensing loop
+static double timeDiff = 0;                     //time difference between two sensing time steps
+static double dist_traversed = 0;               //distance travelled by bot along straight line(+ve for forward and -ve for negative)
+static double angle_rotated = 0;                //angle rotated by bot(+ve for clockwise and -ve for anticlockwise)
+static double lin_vel = 0;                      //linear velocity of the bot
+static double ang_vel = 0;                      //angular velocity of the bot
+static int point_index = 0;                     //index for accessing the appropriate point from points list
+static double accumulated_error = 0;            //integral term in PID formula
+static double current_error = 0;                //current error in PID formula
+static double prev_error = 0;                   //error in the previous time step
+static int pid_flag = 0;                        //flag that signifies that the PID controller's job is done
+static double angle_required = 0;               //angle that the bot needs to rotate to align itself with its destination point
+static double dist_required = 0;                //distance from stop point that the bot needs to travel to reach the destination point
+static int rotating_flag = 1;                   //flag that signifies whether the bot should rotate or move along a straight line
+static double stop_point[2] = {0};              //the position of the bot from which distance travlled is being calculated
+static double prev_point[2] = {0};              //the next point that the bot needs to travel to
+static double current_point[2] = {0};           //current co-ordinates of the bot*/
+static int obstacle_flag[5] = {0};              //1 at the ith index indicates detection of obstacle by sensor[i]
+static double time_flag = 0;                    //used for simulatiing delays
+static int detect_flag = 0;                     //flag to denote whether obstacle has been detected by any of the front 3 sensors
+static double minDist = 0.2;                    //thresholf distance for detection of an obstacle
+static double proxSensDist[5] = {0.5};          //stores distance of obstacle from each sensor
+
+
+/*Placeholder functions*/
+void rotate(double val){
+	return NULL;
+}
+
+void forward(double val){
+	return NULL;
+}
+
+void stop(){
+	return NULL;
+}
+
+/*Creates coordinate array of the path*/
+double* create_coord_array(int n){
+// int main(){
+	int path_number = 0;
+	//int n = 3;
+    int line_length = 0;
+    double val = 0;
+    char c;
+    FILE* f_r = fopen("./test.txt", "r");
+    if(f_r == NULL){
+        printf("Error opening file temp.txt\n");
+    }
+    // Have to handle the invalid path case
+    int counter_to_path_start=0;
+    int space_count = 0;
+    for(;;){
+        if(path_number+1==n){
+            while(1){
+                c = fgetc(f_r);
+                if(c == ' ')
+                    space_count+=1;
+                if( c == EOF || c == '\n' ){
+                    ++line_length;
+                    break;
+                }
+                ++line_length;
+            }
+            fseek(f_r, -(line_length+1), SEEK_CUR);
+            //c = fgetc(f_r);
+            //printf("%c THE CHAR", c);
+            char* path = (char *)calloc(line_length, sizeof(char));
+            strcpy(path, "\0");
+            double* points_array = (double *)calloc((space_count+1), sizeof(double));
+            fgets(path, line_length, f_r);
+            path[line_length-1] = '\0';
+            int current_coord = 0;
+            char* temp_token = strtok(path, "\t");
+            char* temp_path = strstr(path," ");
+            int iters = 0;
+            while(iters<space_count){
+                val = atof(temp_token);//1000.0;
+                points_array[current_coord] = val;
+                printf("%f\t", points_array[current_coord]);
+                current_coord++;
+                temp_token = strtok(temp_path+1, "\t");
+                printf("%s THIS IS TEMP TOK\n" , temp_token);
+            	temp_path = strstr(temp_path+1," ");
+                printf("%s THIS IS TEMP PATH\n" , temp_path);
+                iters++;
+            }
+            printf("%f\n", points_array[current_coord]);
+            points_array[current_coord] = atof(temp_token);
+            printf("%f\n", points_array[current_coord]);
+            printf("%s IM HERE AFTER TOK\n", temp_token);
+            points_len = current_coord;
+            return points_array;
+        }else{
+            for(;;){
+                counter_to_path_start += 1;
+                c = fgetc(f_r);
+                if( c == EOF || c == '\n' )
+                    break;
+            }
+            path_number+=1;
+        }
+        if(c==EOF){
+        	break;
+        }
+    }
+    return NULL;
+}
+
+/*Updates the stop point, should be called everytime the orientation of the bot starts to change*/
+void update_stopPoint(){
+    stop_point[0] = current_point[0];
+    stop_point[1] = current_point[1];
+}
+
+/*Reset the PID variables*/
+void init_pid(){
+    current_error = 0;
+    accumulated_error = 0;
+    prev_error = 0;
+    pid_flag = 0;
+}
+
+/*Calculates the parameters for travelling to from point to next point in path(this is assuming it has reached its previous point correctly)*/
+void update_points(){
+    dist_required = sqrt(pow(points[point_index*2+1] - prev_point[1],2)+pow(points[point_index*2] - prev_point[0],2));
+    if (points[point_index*2]-prev_point[0] >= 0){
+        angle_required = atan((points[point_index*2+1]-prev_point[1])/(points[point_index*2]-prev_point[0]))*180/M_PI;
+    }
+    else{
+        angle_required = -atan((points[point_index*2]-prev_point[0])/(points[point_index*2+1]-prev_point[1]))*180/M_PI;
+        if(points[point_index*2+1]-prev_point[1] >= 0)
+            angle_required = angle_required + 90;
+        else
+            angle_required = angle_required - 90;
+    }
+    prev_point[0] = points[point_index*2];
+    prev_point[1] = points[point_index*2+1];
+    point_index  = point_index + 1;
+    if(point_index >= (points_len+1)/2){
+        point_index = (points_len+1)/2-1;
+        return;
+    }
+    dist_traversed = 0;
+    init_pid();
+}
+
+/*PID Controller for rotating to a specific angle*/
+double get_PID_angle(double current_val, double target_val){
+    current_error = target_val - current_val;
+    accumulated_error = accumulated_error + current_error*timeDiff;
+    double val = 0;
+    if(timeDiff != 0)
+        val = 0.1*current_error + 0.0005*accumulated_error + 0.001*(current_error-prev_error)/timeDiff;
+    else if(timeDiff==0)
+        val = 0.1*current_error + 0.0005*accumulated_error;
+    if(val>5)
+        val = 5;
+    else if(val<-5)
+        val = -5;
+    printf("VALROT: %.17g", val );
+    if(val<0.005&&val>-0.005){
+        pid_flag = 1;
+    }
+    prev_error = current_error;
+    return val;
+}
+
+/*PID Controller for moving along a straight line*/
+double get_PID_dist(double current_val, double target_val){
+    current_error = target_val-current_val;
+    accumulated_error = accumulated_error + current_error*timeDiff; 
+    double val = 500*current_error + 0.1*accumulated_error;
+    if(val > 5)
+        val = 5;
+    else if(val < -5)
+        val = -5;
+    if (val < 0.1&&val>-0.1)
+        pid_flag = 1;
+    prev_error = current_error;
+    return val;
+}
+
+/*When no obstacle is present*/
+void normal_motion(){
+    if(rotating_flag==1){
+        double val = get_PID_angle(angle_rotated, angle_required);
+        if(pid_flag==1){
+            init_pid();
+            stop();
+            rotating_flag = 0;
+        }else{
+            rotate(val);
+        }
+    }else{
+        double val = get_PID_dist(dist_traversed, dist_required);
+        printf("VAL: %.17g\n", val);
+        if(pid_flag==1){
+            init_pid();
+            stop();
+            update_points();
+            update_stopPoint();
+            rotating_flag = 1;
+        }else{
+            forward(val);
+        }
+    }
+}
+
+/*Recalculates the distance to be travelled and the angle to be rotated to reach its destination from its current position*/
+void recalculate(){
+    dist_required = sqrt(pow(prev_point[1]-current_point[1], 2) + pow(prev_point[0]-current_point[0], 2));    
+    if(prev_point[0]-current_point[0] >= 0){
+        angle_required = atan((prev_point[1]-current_point[1])/(prev_point[0]-current_point[0]))*180/M_PI;
+    }
+    else{
+        angle_required = -atan((prev_point[0]-current_point[0])/(prev_point[1]-current_point[1]))*180/M_PI;
+        if(prev_point[1]-current_point[1] >= 0)
+            angle_required = angle_required + 90;
+        else 
+            angle_required = angle_required - 90;
+    }
+    update_stopPoint();
+    dist_traversed = 0;
+    init_pid();
+    rotating_flag = 1;
+}
+
+
+
 struct point{                                   //Data Structure for storing points
     double x;
     double y;
